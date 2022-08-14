1a95b25e4570faf21ebf6dc2f71d9702ae328673
Adebara Ayomide <adebaraayomide.com>
2022-08-14 04:58:35 +0100

    change

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,20 @@
-import "@nomiclabs/hardhat-ethers";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { any } from "hardhat/internal/core/params/argumentTypes";
 require("dotenv").config();
 
-const ALCHEMY_GOERLI_API_KEY_URL = process.env.ALCHEMY_GOERLI;
-
-const ACCOUNT_PRIVATE_KEY = process.env.PRIVATE_KEY;
-
-const API_TOKEN = process.env.API_TOKEN;
-
 type HttpNetworkAccountsUserConfig = any;
-
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     goerli: {
-      url: ALCHEMY_GOERLI_API_KEY_URL,
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY] as HttpNetworkAccountsUserConfig | undefined,
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL,
       accounts: [process.env.PRIVATE_KEY] as HttpNetworkAccountsUserConfig | undefined,
     }
-  },
-  etherscan: {
-    apiKey: API_TOKEN
   }
 };
 
--- scripts/vaultScript.ts
+++ scripts/vaultScript.ts
@@ -1,13 +1,28 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const time = currentTimestampInSeconds + 10;
+
+  //const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+
+  let [acc1, acc2, acc3] = await ethers.getSigners();
 
   const Vault = await ethers.getContractFactory("vault");
   const _vault = await Vault.deploy();
   await _vault.deployed();
 
  
+  //@ts-ignore
+  const amount = ethers.utils.parseEther("1");
+  
+    const res = await _vault.connect(acc2).createGrant(acc2.address,time, {value: amount});
+   const data = await (await res.wait());
+  console.log("value", amount);
+  
   console.log("My compiled vault", _vault.address);
+  console.log("my response", res);
+  console.log("my data", data);
 }
 
 
